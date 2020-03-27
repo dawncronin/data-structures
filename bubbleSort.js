fd0321f519dca0636730ae27e99be0d294587930
dawncronin <dawncronin7@gmail.com>
2020-03-27 14:07:40 -0700

    added bubblesort in Javascript

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+    for(let i = arr.length - 1; i > 0; i--) {
+        for(let j = 0; j <= i; j++) {
+            if (arr[j] > arr[j+1]){
+                let save = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = save
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort([3,4,7,1,6,2,9]))
